2c1d9ba0c759cef2fd5c003e53ead46bc239a606
SakshiiM <137023986+SakshiiM@users.noreply.github.com>
2025-04-24 23:01:21 +0530

    Update demo4_app.py

--- demo4_app.py
+++ demo4_app.py
@@ -12,7 +12,7 @@ def get_db_connection():
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST', 'localhost'),
             user=os.getenv('DB_USER', 'root'),
-            password=os.getenv('DB_PASSWORD', 'Sakshi123'),
+            password=os.getenv('DB_PASSWORD', ' '),
             database=os.getenv('DB_NAME', 'college_chatbot'),
             port=int(os.getenv('DB_PORT', '3307'))
         )
